9837506fa27827d2bafa611295e20cd5273a7f88
nsaspy <nsaspy@fedora.email>
2023-08-05 23:04:49 -0400

    search: fix lots of noob error

--- src/githubForge/search.nim
+++ src/githubForge/search.nim
@@ -11,39 +11,39 @@ import uri
 
 # TODO Include Text match metadata
 # sends the numeric offsets of where the matched term
-proc searchCode*(client: AsyncGithubClient or GithubClient, query: string sort="indexed",
+proc searchCode*(client: AsyncGithubClient or GithubClient, query: string, sort: string = "indexed",
                  order="desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
   ## Search code
   ## Maximum 100 results per page
   ## https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-code
   let url = encodeUrl(makeUrl(fmt"/search/code?q={query}&order={order}&sort={sort}&per_page={perPage}&page={page}"))
-  let req = client.hc.get(url)
+  let req = await client.hc.get(url)
   when defined(debug):
     echo req.status
   castError req
 
   result = (await req.body).parseJson
 
-proc searchCommits*(client: AsyncGithubClient or GithubClient, query: string sort="",
-                 order="desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
+proc searchCommits*(client: AsyncGithubClient or GithubClient, query: string, sort: string = "",
+                 order: string = "desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
   ## Find Commits
   ## Maximum 100 results per page
   ## https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-commits
   let url = encodeUrl(makeUrl(fmt"/search/commits?q={query}&order={order}&sort={sort}&per_page={perPage}&page={page}"))
-  let req = client.hc.get(url)
+  let req = await client.hc.get(url)
   when defined(debug):
     echo req.status
   castError req
 
   result = (await req.body).parseJson
 
-proc searchIssues*(client: AsyncGithubClient or GithubClient, query: string sort="",
-                 order="desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
+proc searchIssues*(client: AsyncGithubClient or GithubClient, query: string, sort: string = "",
+                 order: string ="desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
   ## Search For Issues
   ## Maximum of 100 results per page
   ## https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-issues-and-pull-requests
   let url = encodeUrl(makeUrl(fmt"/search/issues?q={query}&order={order}&sort={sort}&per_page={perPage}&page={page}"))
-  let req = client.hc.get(url)
+  let req = await client.hc.get(url)
   when defined(debug):
     echo req.status
   castError req
@@ -51,13 +51,13 @@ proc searchIssues*(client: AsyncGithubClient or GithubClient, query: string sort
   result = (await req.body).parseJson
 
 
-proc searchLabels*(client: AsyncGithubClient or GithubClient, query: string sort="",
-                 order="desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
+proc searchLabels*(client: AsyncGithubClient or GithubClient, query: string, sort: string = "",
+                 order: string = "desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
   ## Find Labels in a repo
   ## Maximum of 100 results per page
   ## https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-labels
   let url = encodeUrl(makeUrl(fmt"/search/labels?q={query}&order={order}&sort={sort}&per_page={perPage}&page={page}"))
-  let req = client.hc.get(url)
+  let req = await client.hc.get(url)
   when defined(debug):
     echo req.status
   castError req
@@ -65,13 +65,13 @@ proc searchLabels*(client: AsyncGithubClient or GithubClient, query: string sort
   result = (await req.body).parseJson
 
 
-proc searchRepos*(client: AsyncGithubClient or GithubClient, repoId: int, query: string sort="",
-                 order="desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
+proc searchRepos*(client: AsyncGithubClient or GithubClient, repoId: int, query: string, sort: string = "",
+                 order: string = "desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
   ## Search For repositories
   ## Maximum of 100 results per page
   ## https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-repositories
   let url = encodeUrl(makeUrl(fmt"/search/repositories?repository_id={repoId}&q={query}&order={order}&sort={sort}&per_page={perPage}&page={page}"))
-  let req = client.hc.get(url)
+  let req = await client.hc.get(url)
   when defined(debug):
     echo req.status
   castError req
@@ -80,13 +80,13 @@ proc searchRepos*(client: AsyncGithubClient or GithubClient, repoId: int, query:
 
 
 
-proc searchTopics*(client: AsyncGithubClient or GithubClient, query: string sort="",
-                 order="desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
+proc searchTopics*(client: AsyncGithubClient or GithubClient, query: string, sort: string = "",
+                 order: string = "desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
   ## Search topics
   ## Maximum of 100 results per page
   ## https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-topics
   let url = encodeUrl(makeUrl(fmt"/search/topics?q={query}&order={order}&sort={sort}&per_page={perPage}&page={page}"))
-  let req = client.hc.get(url)
+  let req = await client.hc.get(url)
   when defined(debug):
     echo req.status
   castError req
@@ -94,14 +94,14 @@ proc searchTopics*(client: AsyncGithubClient or GithubClient, query: string sort
   result = (await req.body).parseJson
 
 
-proc searchUsers*(client: AsyncGithubClient or GithubClient, query: string sort="",
-                 order="desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
+proc searchUsers*(client: AsyncGithubClient or GithubClient, query: string, sort: string = "",
+                 order: string = "desc", page=1, perPage=30): Future[JsonNode] {.multisync.} =
   ## Find Users
   ## Maximum of 100 results per page
   ## https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-users
 
   let url = encodeUrl(makeUrl(fmt"/search/users?q={query}&order={order}&sort={sort}&per_page={perPage}&page={page}"))
-  let req = client.hc.get(url)
+  let req = await client.hc.get(url)
   when defined(debug):
     echo req.status
   castError req
